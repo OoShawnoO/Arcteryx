dc2d272037a736af4c91f0178ab769f5bc95e36b
Clomu <78494317+OoShawnoO@users.noreply.github.com>
2021-11-24 22:20:56 +0800

    Update Delete.java

--- src/swu/edu/hzd/Delete.java
+++ src/swu/edu/hzd/Delete.java
@@ -7,13 +7,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 @WebServlet(name="Delete")
 public class Delete extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response){
         SQLtool sqLtool = new SQLtool();
+        ArrayList<Goods> arrayList = new ArrayList<>();
         int id = Integer.valueOf(request.getParameter("delete"));
         try {
+            arrayList = sqLtool.Select();
+            int i=0;
+            for(i=0;i<arrayList.size();i++){
+                if(arrayList.get(i).getId()==id){
+                    sqLtool.Insert("delete_history",arrayList.get(i).getName(), String.valueOf(request.getSession().getAttribute("username")),arrayList.get(i).getPrice(),arrayList.get(i).getCost());
+                }
+            }
             sqLtool.Delete(id);
             response.sendRedirect("recordlist.jsp");
         } catch (SQLException | IOException e) {
