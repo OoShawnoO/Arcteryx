a3ed9451e8cca03625f94336247926e26938f10e
Clomu <78494317+OoShawnoO@users.noreply.github.com>
2021-11-24 22:18:02 +0800

    Update Team.java

--- src/swu/edu/hzd/Team.java
+++ src/swu/edu/hzd/Team.java
@@ -19,7 +19,9 @@ public class Team extends HttpServlet {
         response.setContentType("text/html");
         response.setCharacterEncoding("UTF-8");
         PrintWriter out = response.getWriter();
-
+        out.println("<h1 style='text-align:center;'>当你看到这个网站的时候可能我就要提醒您</h1>");
+        out.println("<h1 style='text-align:center'>您没有本站的管理员权限哦，没有办法对数据进行修改。</h1>");
+        out.println("<center><button style='width:100px;height:80px;' onclick='window.location.href=\"/RecordSystem/index.jsp\"'>主页</a></center>");
         out.println("<marquee style='margin-top:15%;' direction=\"right\"><img src=\"/image/2.gif\" style='width=100px;height:100px;'><p style=\"font-family: 'Microsoft JhengHei Light ';\">没错就是本人写的</p></marquee>");
     }
 
