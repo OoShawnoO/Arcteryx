1ef922be8261680b9a7cc06f52042318b3d9d534
OoShawnoO <840831038@qq.com>
2021-11-26 14:23:04 +0800

    添加输入信息筛选功能

--- src/swu/edu/hzd/UpdateFilter.java
+++ src/swu/edu/hzd/UpdateFilter.java
@@ -5,6 +5,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class UpdateFilter implements Filter {
 
@@ -14,10 +15,28 @@ public class UpdateFilter implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        if(!(request.getSession().getAttribute("username")).equals(ADMIN_USERNAME)){
+
+        if(request.getServletPath().equals("/Update")){
+            try {
+                Float.valueOf(request.getParameter("price"));
+                Float.valueOf(request.getParameter("cost"));
+            }catch (NumberFormatException e){
+                response.sendRedirect("Revise.jsp?wrong=-1");
+            }
+
+        }
+        else if(request.getServletPath().equals("/AddGoods")){
+            try{
+                Float.valueOf(request.getParameter("input2"));
+                Float.valueOf(request.getParameter("input3"));
+            }catch (NumberFormatException e){
+                response.sendRedirect("Add.html?wrong=-1");
+            }
+        }
+
+        else if(!(request.getSession().getAttribute("username")).equals(ADMIN_USERNAME)){
             if(!response.isCommitted()){
                 response.sendRedirect("/RecordSystem/team");
-
             }
         }
         else{
--- web/Revise.jsp
+++ web/Revise.jsp
@@ -39,6 +39,21 @@
   <link href="static/assets/css/style.css" rel="stylesheet">
 
   <script type="text/javascript">
+    function getQueryVariable(variable)
+    {
+      var query = window.location.search.substring(1);
+      var vars = query.split("&");
+      for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+      }
+      return(false);
+    }
+
+    if(getQueryVariable("wrong")=="-1"){
+      alert("输入数据类型错误！")
+    }
+
 
 
   </script>
